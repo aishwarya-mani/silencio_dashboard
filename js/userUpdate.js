c9591de042eec86503f40c6812b04200abce28f1
Kaustubh Srivastava <Kaustubh Srivastava>
2017-04-01 17:01:28 +0530

    4

---
+++ js/userUpdate.js
@@ -0,0 +1,40 @@
+var btn = document.getElementById("update")
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("btnAdd");
+var editBtn = document.getElementByID("edit");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//Get the button that adds the card
+var submit = document.getElementById("submit");
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+//when the user clicks the edit button on the card
+editBtn.onclick = function(){
+    modal.style.display = "block";
+}
+
+//adding user info card when submit button is clicked
+submit.onclick = function(){
+    document.getElementById("userCard").style.display = "block";
+}
